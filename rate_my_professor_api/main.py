db36d850d8c949c7fec6f61acdd3bb5a9057aba0
Jonathan <Jportorreal77@gmail.com>
2016-10-15 00:58:22 -0400

    we can get teachers overall rating now

--- rate_my_professor_api/main.py
+++ rate_my_professor_api/main.py
@@ -7,8 +7,8 @@ SEARCH_ENDPOINT = 'search.jsp?query='
 DATA_ENDPOINT = 'ShowRatings.jsp?tid='
 
 # HARD CODED
-name_of_professor = 'david'
-college_of_student = 'Queens College, Anthropology'
+name_of_professor = 'Daniel, David B.'
+college_of_student = 'James Madison University, Psychology'
 # HARD CODED
 
 
@@ -20,24 +20,34 @@ tree = html.fromstring(page.content)
 professors = tree.xpath('//span[@class="main"]/text()')
 colleges = tree.xpath('//span[@class="sub"]/text()')
 tid = tree.xpath('//li[@class="listing PROFESSOR"]/a/@href')
-print 'tid', tid
 # print 'professors', professors
 # print 'colleges', colleges
 
 
 # dictionary keys are colleges values are dicts of {professor: search_tid}
 students_professor_dict = {}
-for i in range(len(professors)):
-    students_professor_dict[colleges[i]] = professors[i]
-# students_professor = dict(zip(colleges, zip(professors, tid)))
 
+for i in range(len(professors)):
+    students_professor_dict[colleges[i]] = dict([(professors[i], tid[i])])
+
+# students_professor = dict(zip(colleges, zip(professors, tid)))
+"""
 for keys, values in students_professor_dict.items():
     print ('key', keys)
     print ('value', values)
+"""
 
 # logic here
 #---------------------------------------------------
-"""
+prof_tid = students_professor_dict[
+    college_of_student][name_of_professor]
+
 profile_page = requests.get(
-    BASE_URL + students_professor[])
-"""
+    BASE_URL + prof_tid)
+
+
+profile_tree = html.fromstring(profile_page.content)
+grade = profile_tree.xpath(
+    '//div[@class="breakdown-container"]/div/div/text()')[0]
+
+print grade
