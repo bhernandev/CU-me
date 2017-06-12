9415ce61bd276512b19278085a1efb33233f2b81
Maximilian Ioffe <maxioffe@gmail.com>
2017-06-11 20:22:23 -0400

    Clean files

--- cume/config/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^cumeadmaxi/', admin.site.urls),
-    url(r'^api/', include('api.urls')),
-    url(r'^', include('schedule.urls')),
-]
